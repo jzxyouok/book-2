008abf10288e09950447656dda7dca83b97fbda5
vincent <apanly@163.com>
2017-02-27 22:24:12 +0800

    vincent  -- save

--- sync.sh
+++ sync.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-git fetch origin && git rebase origin/master && git push origin master:master
-git fetch oschina && git rebase oschina/master && git push oschina master:master
+git push origin master:master && git push oschina master:master
 echo 'push over'
