fab4ed16bad967b69cb2e34d0181346988aba2c3
vincent <apanly@163.com>
2017-03-02 22:45:35 +0800

    vincent -- 1

--- modules/m/views/layouts/main.php
+++ modules/m/views/layouts/main.php
@@ -8,6 +8,7 @@ use \app\common\services\UrlService;
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<!-- Set render engine for 360 browser -->
 	<meta name="renderer" content="webkit">
 	<title>微店铺首页</title>
